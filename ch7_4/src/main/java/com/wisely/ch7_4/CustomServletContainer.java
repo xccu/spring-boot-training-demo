f6b822a10f2dfa63c9497e82705428c9081935b1
xccu <264672790@qq.com>
2020-09-14 20:59:45 +0800

    added comment

--- ch7_4/src/main/java/com/wisely/ch7_4/CustomServletContainer.java
+++ ch7_4/src/main/java/com/wisely/ch7_4/CustomServletContainer.java
@@ -13,12 +13,13 @@ public class CustomServletContainer implements EmbeddedServletContainerCustomize
 
 	@Override
 	public void customize(ConfigurableEmbeddedServletContainer container) {
-		//1.配置端口号
+/*		//1.配置端口号
 		container.setPort(8888);
 		//2.配置错误页面，根据httpstatus错误状态信息，跳转到指定的错误页面
 		container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404.html"));
 		//3.配置Servlet容器session过期时间
 		container.setSessionTimeout(10,TimeUnit.MINUTES);
+		*/
 	}
 
 }
