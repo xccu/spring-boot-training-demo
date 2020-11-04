4ea27f24348e37a7a22e833793c5eaea06932ae7
xccu <264672790@qq.com>
2020-11-04 19:39:54 +0800

    added comment

--- ch7_7/src/main/resources/static/js-action/app.js
+++ ch7_7/src/main/resources/static/js-action/app.js
@@ -1,17 +1,17 @@
 /**
- * 
+ *  多视图和路由
  */
 
-var actionApp = angular.module('actionApp',['ngRoute']);
+var actionApp = angular.module('actionApp',['ngRoute']);  //1.定义模块actionApp，并依赖于路由模块ngRout
 
 
 
 
-actionApp.config(['$routeProvider' , function($routeProvider) {
+actionApp.config(['$routeProvider' , function($routeProvider) { //2.配置路由，并注入$$routeProvider来配置
 	
-	$routeProvider.when('/oper', { 
-		controller: 'View1Controller', 
-		templateUrl: 'views/view1.html', 
+	$routeProvider.when('/oper', {          //3. oper为路由名称
+		controller: 'View1Controller',      //4. controller定义的是路由的控制器名称
+		templateUrl: 'views/view1.html',    //5.templateUrl定义视图的真正地址
 	}).when('/directive', {
 		controller: 'View2Controller',
 		templateUrl: 'views/view2.html',
